d46d01144ddada0534c16f1fefcbbac63bedecfb
김주홎 <gyong0812@naver.com>
2018-11-15 16:07:24 +0900

    function pointer

--- main.c
+++ main.c
@@ -1,18 +1,74 @@
 #include <stdio.h>
 
-char *proverb="All that glisters is not gold.";
 
+int addfunc(int a,int b);
+int subfunc(int a, int b);
+int mulfunc(int a, int b);
+int divfunc(int a, int b);
 
-void setPointer(char **q) 
-{
-	*q = proverb;
-}
 
 int main(void)
 {
-	char *p="zzz";
-	setPointer(&p);
-	printf("%s\n",p);
+	
+	int num1;
+	int num2;
+	char op;
+	
+	int (*calcfunc)(int, int); //int를 반환하는 함수포인터  
+	
+	
+	printf("input calculation : ");
+	scanf("%d %c %d",&num1, &op, &num2);
+	
+	
+	switch(op)
+	{
+		case '+' : 
+			calcfunc = addfunc;
+			break;
+		case '-' : 
+			calcfunc = subfunc;
+			break;
+		case '*' : 
+			calcfunc = mulfunc;
+			break;
+		case '/' : 
+			calcfunc = divfunc;
+			break;	
+	}
+	
+	
+	printf("result = %i\n",calcfunc(num1, num2));
 	
 	return 0;
+
 }
+
+
+
+
+
+int addfunc(int a, int b)
+{
+	return a+b;
+}
+
+
+int subfunc(int a, int b)
+{
+	return a-b;
+}
+
+int mulfunc(int a, int b)
+{
+	return a*b;
+}
+
+int divfunc(int a, int b)
+{
+	return a/b;
+}
+
+
+
+
